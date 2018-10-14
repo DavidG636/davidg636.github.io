ae41d7d8af62d0d1cf39936b0620071c9cf5611a
David Gonzalez <gonzalezdavid636@gmail.com>
2018-10-13 22:59:19 -0400

    Add web image and git image that are used in project section for watchLater project

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.watchLaterThumb').hover(function(){ //Open on hover
+        $('#overlay').css('display', 'block');
+    },
+    function(){ //Close when not hovered
+        $('#overlay').css('display', 'none');
+    });
+
+    $('.gitPicWatchLater').click(function () {
+      window.open('https://github.com/DavidG636/Youtube-Watch-Later-Player', '_blank');
+    });
+
+    $('.webPicWatchLater').click(function () {
+      window.open('https://davidg636.github.io/Youtube-Watch-Later-Player/', '_blank');
+    });
+});
