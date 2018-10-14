544c01f443a720d1010400b54743097e92fabe96
David Gonzalez <gonzalezdavid636@gmail.com>
2018-10-13 23:50:53 -0400

    Add case project section

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ $(function() {
     $('.webPicWatchLater').click(function () {
       window.open('https://davidg636.github.io/Youtube-Watch-Later-Player/', '_blank');
     });
+
+    $('.gitPicCase').click(function () {
+      window.open('https://github.com/DavidG636/Case', '_blank');
+    });
+
+    $('.webPicCase').click(function () {
+      window.open('https://davidg636.github.io/Case/', '_blank');
+    });
 });
