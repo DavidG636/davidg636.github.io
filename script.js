b6ef5e231188e3755ca9bf3a80531de5c9e0cc5c
DavidG636 <gonzalezdavid636@gmail.com>
2018-10-18 14:51:02 -0400

    Add lowercase project

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 $(function() {
 
-    $('.gitPicWatchLater').click(function () {
-      window.open('https://github.com/DavidG636/Youtube-Watch-Later-Player', '_blank');
-    });
+  $('.gitPicWatchLater').click(function() {
+    window.open('https://github.com/DavidG636/Youtube-Watch-Later-Player', '_blank');
+  });
 
-    $('.webPicWatchLater').click(function () {
-      window.open('https://davidg636.github.io/Youtube-Watch-Later-Player/', '_blank');
-    });
+  $('.webPicWatchLater').click(function() {
+    window.open('https://davidg636.github.io/Youtube-Watch-Later-Player/', '_blank');
+  });
 
-    $('.gitPicCase').click(function () {
-      window.open('https://github.com/DavidG636/Case', '_blank');
-    });
+  $('.gitPicCase').click(function() {
+    window.open('https://github.com/DavidG636/Case', '_blank');
+  });
 
-    $('.webPicCase').click(function () {
-      window.open('https://davidg636.github.io/Case/', '_blank');
-    });
+  $('.webPicCase').click(function() {
+    window.open('https://davidg636.github.io/Case/', '_blank');
+  });
 
-    $('.gitPicParagraphUtilities').click(function () {
-      window.open('https://github.com/DavidG636/Paragraph-Utilities', '_blank');
-    });
+  $('.gitPicParagraphUtilities').click(function() {
+    window.open('https://github.com/DavidG636/Paragraph-Utilities', '_blank');
+  });
 
-    $('.webPicParagraphUtilities').click(function () {
-      window.open('https://davidg636.github.io/Paragraph-Utilities/', '_blank');
-    });
+  $('.webPicParagraphUtilities').click(function() {
+    window.open('https://davidg636.github.io/Paragraph-Utilities/', '_blank');
+  });
+  $('.gitPicLowercase').click(function() {
+    window.open('https://github.com/DavidG636/Lowercase', '_blank');
+  });
+
+  $('.webPicLowercase').click(function() {
+    window.open('https://davidg636.github.io/Lowercase/', '_blank');
+  });
 });
