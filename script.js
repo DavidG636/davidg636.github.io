03ce82edf02435fcfd225f9feac84dd937a813f6
David Gonzalez <gonzalezdavid636@gmail.com>
2018-11-11 20:28:46 -0500

    Add typewriter to what I am section and ass projects to navbar

--- script.js
+++ script.js
@@ -46,4 +46,51 @@ $(function() {
   $('.filePicHelloName').click(function() {
     window.open('https://github.com/DavidG636/helloName/blob/master/helloName.c', '_blank');
   });
+
+  class Typewriter {
+    constructor(element, words, wait = 3000) {
+      this.element = element;
+      this.words = words;
+      this.text = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+
+    type() {
+
+      const current = this.wordIndex % this.words.length;
+      const fullTxt = this.words[current];
+
+      if (this.isDeleting) {
+        this.text = fullTxt.substring(0, this.text.length - 1);
+      } else {
+        this.text = fullTxt.substring(0, this.text.length + 1);
+      }
+
+      this.element.html(`<span class="txt">${this.text}</span>`);
+      let speed = 300;
+
+      if (this.isDeleting) {
+        speed /= 2;
+      }
+
+      if (!this.isDeleting && this.text === fullTxt) {
+        speed = this.wait;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.text === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+        speed = 500;
+      }
+
+      setTimeout(() => {this.type()}, speed);
+    }
+  }
+
+  const element = $('.typewriter');
+  const words = JSON.parse($(element).attr('data-words'));
+  const wait = $(element).attr('data-wait');
+  var creator = new Typewriter(element, words, wait);
 });
